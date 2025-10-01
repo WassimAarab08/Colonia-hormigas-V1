d6a7f080842bdf04b89f27fbe3a20978e6755be4
wassi <wassimaarab18@gmail>
2025-10-01 15:56:08 +0200

    Clase hormiga terminada

---
+++ src/Hormiga.java
@@ -0,0 +1,43 @@
+import java.util.Random;
+
+public class Hormiga {
+
+    protected final String id;
+    protected final TipoHormiga tipo;
+    protected volatile Posicion posicion;
+    protected volatile boolean activa;
+    protected final Random random;
+    protected static final int[][] DIRECCIONES = new int[4][4];
+
+
+    public Hormiga(String id, TipoHormiga tipo, Random random) {
+        this.id = id;
+        this.tipo = tipo;
+        this.random = random;
+    }
+
+    public String getIdHormiga(){
+        return id;
+    }
+
+    public TipoHormiga getTipo(){
+        return tipo;
+    }
+
+    public Posicion getPosicion(){
+        return posicion;
+    }
+
+    public void setPosicion(Posicion nuevaPosicion){
+        posicion = nuevaPosicion;
+    }
+
+    public boolean isActiva(){
+        return activa;
+    }
+
+
+    public void detener(){
+        activa = false;
+    }
+}
