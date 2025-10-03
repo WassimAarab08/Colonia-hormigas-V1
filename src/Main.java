6817cc28052731b7810e10478f240741b5dd3248
wassi <wassimaarab18@gmail>
2025-10-03 11:40:13 +0200

    Implementado funcionamiento de imprimir el mapa incial

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,7 @@
 public class Main {
+
+    public static void main(String[] args) {
+        Mapa m1 = new Mapa();
+        m1.mostrarMapa();
+    }
 }
--- src/Mapa.java
+++ src/Mapa.java
@@ -1,15 +1,25 @@
 import java.util.HashMap;
 
 public class Mapa {
-    public static final int ANCHO= 10;
+    public static final int ANCHO= 9;
     public static final int ALTO =5;
-    private final Posicion hormiguero = new Posicion(2,5)  ;
-    private final char[][] mapa = new char[ANCHO][ALTO];
+    private final Posicion hormiguero  ;
+    private final char[][] mapa ;
     public static final char VACIO = '*';
-    public static final char HORMIGUERO = 'o';
+    public static final char HORMIGUERO = 'O';
 
 
     public Mapa() {
+        mapa = new char[ALTO][ANCHO];
+        for (int i = 0; i < ALTO; i++) {
+            for (int j = 0; j < ANCHO; j++) {
+                mapa[i][j] = VACIO;
+            }
+        }
+
+        hormiguero = new Posicion(ALTO / 2, ANCHO / 2);
+        mapa[hormiguero.getY()][hormiguero.getX()] = HORMIGUERO;
+
     }
 
     public Posicion getHormiguero(){
@@ -19,11 +29,18 @@ public class Mapa {
        return posicion.dentroLimites(ANCHO, ALTO);
     }
     public synchronized void mostrarMapa(){
+        for (int i = 0; i < ALTO; i++) {
+            for (int j = 0; j < ANCHO; j++) {
+                System.out.printf(mapa[i][j]+" ");
+            }
+            System.out.println();
+        }
 
     }
 
 
     public void prepararMapa(HashMap<String, Hormiga> hormigas){
+
 
     }
 }
